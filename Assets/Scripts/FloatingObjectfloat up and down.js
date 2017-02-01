40d44e8264f4675c8a098341cf03f390a9b11d02
malditomoraes <raphaelmoraes025@gmail.com>
2017-02-01 13:01:52 -0400

    Algumas mudanças - textura da parede, totem afins

---
+++ Assets/Scripts/FloatingObjectfloat up and down.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+ var floatup;
+ function Start(){
+     floatup = false;
+ }
+ function Update(){
+     if(floatup)
+         floatingup();
+     else if(!floatup)
+         floatingdown();
+ }
+ function floatingup(){
+     transform.position.y += 0.05 * Time.deltaTime;
+     yield WaitForSeconds(3);
+     floatup = false;
+ }
+ function floatingdown(){
+     transform.position.y -= 0.05 * Time.deltaTime;;
+     yield WaitForSeconds(3);
+     floatup = true;
+ }
---
+++ Assets/Scripts/luz de velas.js
@@ -0,0 +1,18 @@
+﻿
+
+var FlashingLight : Light;
+FlashingLight.enabled = false;
+
+
+function Update () {
+
+var RandomNumber = Random.value;
+
+if(RandomNumber<=.9){
+FlashingLight.enabled = true;
+
+}
+else FlashingLight.enabled= false;
+
+}
+
